1b1f673e8509f5058f493be097e039ce3cbad21c
AlexPetushkov <petushkov8485@gmail.com>
2024-02-15 03:13:06 +0700

    Добавлена возможность сохранения созданных и изменённых файлов.

--- wpfCopilator/CreateNewWindow.xaml.cs
+++ wpfCopilator/CreateNewWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,6 +30,12 @@ namespace wpfCopilator
         private void CreateButton_Click(object sender, RoutedEventArgs e)
         {
             FileName = myTextBox.Text;
+            File.Create(Environment.CurrentDirectory + "//" + "tempFilesDirectory//" + FileName + ".txt");
+            this.Close();
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
             this.Close();
         }
     }
